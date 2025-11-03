44b52a9cf557019ff0e1d19e7b74ebf4220988fd
analystflynbeds25 <flynbeds.it@gmail.com>
2025-11-03 10:47:03 +0100

    adding existing code 2

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -8,26 +8,9 @@ interface LogoProps {
 export const Logo: React.FC<LogoProps> = ({ className }) => {
   return (
     <div className={`inline-block ${className}`}>
-      <svg viewBox="0 0 160 100" className="w-full h-full">
-        <defs>
-          <style>
-            {`.nouba-text { font-family: 'Poppins', sans-serif; font-weight: 700; font-size: 28px; fill: #1e3a8a; }`}
-          </style>
-        </defs>
+      <svg viewBox="0 0 160 100" className="w-full h-full" src="C:\Users\HTL-ADM\Downloads\nouba-plus-lead-capture\components\NOUBA_PLUS_LOGO.svg"/>
         
-        {/* Orange Logo Shape */}
-        <g fill="#F97316">
-          {/* Outer Arch */}
-          <path d="M40,70 V30 C40,10 120,10 120,30 V70 H100 V35 C100,25 60,25 60,35 V70 H40 Z" />
-          {/* Inner Arch */}
-          <path d="M70,60 V40 C70,35 90,35 90,40 V60 H70 Z" />
-        </g>
-        
-        {/* Text */}
-        <text x="50%" y="95" textAnchor="middle" className="nouba-text">
-          NOUBA PLUS
-        </text>
-      </svg>
+    
     </div>
   );
 };
